1510e0a525f164467300091c757b8884fd5c8921
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-06-11 00:09:16 -0600

    :wrench: Config vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import robotsTxt from "astro-robots-txt";
+import { defineConfig } from 'astro/config'
 
-import react from "@astrojs/react";
+import tailwind from '@astrojs/tailwind'
+import robotsTxt from 'astro-robots-txt'
+import react from '@astrojs/react'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt(), react()]
-});
+  integrations: [
+    tailwind(),
+    robotsTxt(),
+    react()
+  ],
+  output: 'server',
+  adapter: vercel()
+})
