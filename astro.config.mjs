fef4ed22b3e2f1522855af54f93ec79def1cbe84
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-06-10 23:34:06 -0600

    :sparkles: Add dependencies and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [tailwind(), robotsTxt(), react()]
 });
