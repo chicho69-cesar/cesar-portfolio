06bbe991f1257261d1860e9ac5a4fd21d763abba
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-07-07 19:43:26 -0600

    :sparkles: Add certifications data

---
+++ src/data/certifications.ts
@@ -0,0 +1,68 @@
+export const CERTIFICATIONS = {
+  'es': [
+    {
+      title: 'AWS Certified Cloud Practitioner',
+      emittedBy: 'Amazon Web Services',
+      issueDate: '13-06-2024',
+      expirationDate: '13-06-2027',
+      image: '/certifications/aws-cloud-practitioner.webp',
+    },
+    {
+      title: 'CCNAv7: Introduction to Networks',
+      emittedBy: 'Cisco',
+      issueDate: '30-08-2022',
+      image: '/certifications/ccna-introduction-to-networks.webp',
+    },
+    {
+      title: 'CCNAv7: Switching, Routing, and Wireless Essentials',
+      emittedBy: 'Cisco',
+      issueDate: '28-11-2022',
+      image: '/certifications/ccna-switching-routing-and-wireless-essentials.webp',
+    },
+    {
+      title: 'CCNAv7: Enterprise Networking, Security, and Automation',
+      emittedBy: 'Cisco',
+      issueDate: '31-05-2023',
+      image: '/certifications/ccna-enterprise-networking-security-and-automation.webp',
+    },
+    {
+      title: 'CCNAv7: Network Security',
+      emittedBy: 'Cisco',
+      issueDate: '16-05-2024',
+      image: '/certifications/ccna-network-security.webp',
+    },
+  ],
+  'en': [
+    {
+      title: 'AWS Certified Cloud Practitioner',
+      emittedBy: 'Amazon Web Services',
+      issueDate: '13-06-2024',
+      expirationDate: '13-06-2027',
+      image: '/certifications/aws-cloud-practitioner.webp',
+    },
+    {
+      title: 'CCNAv7: Introduction to Networks',
+      emittedBy: 'Cisco',
+      issueDate: '30-08-2022',
+      image: '/certifications/ccna-introduction-to-networks.webp',
+    },
+    {
+      title: 'CCNAv7: Switching, Routing, and Wireless Essentials',
+      emittedBy: 'Cisco',
+      issueDate: '28-11-2022',
+      image: '/certifications/ccna-switching-routing-and-wireless-essentials.webp',
+    },
+    {
+      title: 'CCNAv7: Enterprise Networking, Security, and Automation',
+      emittedBy: 'Cisco',
+      issueDate: '31-05-2023',
+      image: '/certifications/ccna-enterprise-networking-security-and-automation.webp',
+    },
+    {
+      title: 'CCNAv7: Network Security',
+      emittedBy: 'Cisco',
+      issueDate: '16-05-2024',
+      image: '/certifications/ccna-network-security.webp',
+    },
+  ]
+} as any
