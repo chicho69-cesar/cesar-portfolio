bdf38c062251809342e2165032856e0a8e390bd2
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-06-22 11:38:36 -0600

    :sparkles: Add islands components

--- src/islands/Meteors.tsx
+++ src/islands/Meteors.tsx
@@ -1,43 +1,43 @@
-'use client'
+"use client"
 
-import clsx from 'clsx'
-import { useEffect, useState } from 'react'
+import { cn } from "@/lib/utils"
+import { motion } from "motion/react"
 
 interface MeteorsProps {
   number?: number
+  className?: string
 }
 
-export const Meteors = ({ number = 20 }: MeteorsProps) => {
-  const [meteorStyles, setMeteorStyles] = useState<Array<React.CSSProperties>>(
-    [],
-  )
-
-  useEffect(() => {
-    const styles = [...new Array(number)].map(() => ({
-      top: -5,
-      left: Math.floor(Math.random() * window.innerWidth) + 'px',
-      animationDelay: Math.random() * 1 + 0.2 + 's',
-      animationDuration: Math.floor(Math.random() * 8 + 2) + 's',
-    }))
-
-    setMeteorStyles(styles)
-  }, [number])
-
+export default function Meteors({ number, className }: MeteorsProps) {
+  const meteors = new Array(number || 20).fill(true)
+  
   return (
-    <>
-      {[...meteorStyles].map((style, idx) => (
-        <span
-          key={idx}
-          className={clsx(
-            'pointer-events-none absolute left-1/2 top-1/2 h-0.5 w-0.5 rotate-[215deg] animate-meteor rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10]',
-          )}
-          style={style}
-        >
-          <div className='pointer-events-none absolute top-1/2 -z-10 h-[1px] w-[50px] -translate-y-1/2 bg-gradient-to-r from-slate-500 to-transparent' />
-        </span>
-      ))}
-    </>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      {meteors.map((el, idx) => {
+        const meteorCount = number || 20
+        const position = idx * (800 / meteorCount) - 400
+
+        return (
+          <span
+            key={'meteor' + idx}
+            className={cn(
+              'animate-meteor-effect absolute h-0.5 w-0.5 rotate-[45deg] rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10]',
+              'before:absolute before:top-1/2 before:h-[1px] before:w-[50px] before:-translate-y-[50%] before:transform before:bg-gradient-to-r before:from-[#64748b] before:to-transparent before:content-[""]',
+              className,
+            )}
+            style={{
+              top: '-40px',
+              left: position + 'px',
+              animationDelay: Math.random() * 5 + 's',
+              animationDuration: Math.floor(Math.random() * (10 - 5) + 5) + 's',
+            }}
+          ></span>
+        )
+      })}
+    </motion.div>
   )
 }
-
-export default Meteors
