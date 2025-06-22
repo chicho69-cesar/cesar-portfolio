6b0297cec54781a460efaa63c473830bf6a565a3
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-06-22 13:42:05 -0600

    :sparkles: Finish translations

--- src/i18n/sections.ts
+++ src/i18n/sections.ts
@@ -1,3 +1,45 @@
+export const hero = {
+  'es': {
+    greeting: 'Hola, soy',
+    available: 'Disponible para trabajar',
+    experience: '+1,5 años de experiencia.',
+    description: 'Apasionado Ingeniero de Software y desarrollador Full-Stack, especializado en crear aplicaciones web y móviles únicas que transforman la forma en que las personas interactúan con la tecnología.',
+    download: 'Descargar CV',
+  },
+  'en': {
+    greeting: 'Hi, I am',
+    available: 'Available for work',
+    experience: '+1.5 years of experience.',
+    description: 'Passionate Software Engineer and Full-Stack developer, specialized in creating unique web and mobile applications that transform the way people interact with technology.',
+    download: 'Download CV',
+  },
+}
+
+export const aboutMe = {
+  'es': {
+    paragraph1: '¡Hola! Soy Cesar Villalobos Olmos, aunque mis amigos me llaman Chicho. Soy un apasionado desarrollador Full-Stack e Ingeniero en TIC\'s. Mi curiosidad y afición por los videojuegos me llevaron a descubrir el mundo de la programación, y hoy en día',
+    paragraph1Highlight1: 'trabajo en el desarrollo por pasión y amor a la tecnología.',
+    paragraph2Sec1: 'Gracias a la programación he tenido la oportunidad de',
+    paragraph2Highlight1: 'vivir experiencias increíbles, conocer personas maravillosas y estar lugares únicos,',
+    paragraph2Sec2: 'he trabajado en proyectos como freelance, con equipos en entornos empresariales y he tenido la oportunidad de competir en diversos concursos y hackatones que me han ayudado a llegar hasta donde estoy hoy.',
+    paragraph3Highlight1: 'Me encanta compartir conocimientos y aprender de otros,',
+    paragraph3Sec1: 'siempre he creído firmemente que el trabajo en equipo es la clave del éxito. Si estás buscando a alguien con',
+    paragraph3Highlight2: 'pasión, creatividad y mucha energía,',
+    paragraph3Sec2: 'me encantaría conectar contigo y ver cómo puedo contribuir a tu próximo proyecto.'
+  },
+  'en': {
+    paragraph1: 'Hi! I am Cesar Villalobos Olmos, but my friends call me Chicho. I am a passionate Full-Stack developer and IT Engineer. My curiosity and love for video games led me to discover the world of programming, and today',
+    paragraph1Highlight1: 'I work in development out of passion and love for technology.',
+    paragraph2Sec1: 'Thanks to programming, I have had the opportunity to',
+    paragraph2Highlight1: 'live incredible experiences, meet wonderful people, and be in unique places,',
+    paragraph2Sec2: 'I have worked on freelance projects, with teams in corporate environments, and I have had the chance to compete in various contests and hackathons that have helped me get to where I am today.',
+    paragraph3Highlight1: 'I love sharing knowledge and learning from others,',
+    paragraph3Sec1: 'I have always firmly believed that teamwork is the key to success. If you are looking for someone with',
+    paragraph3Highlight2: 'passion, creativity, and lots of energy,',
+    paragraph3Sec2: 'I would love to connect with you and see how I can contribute to your next project.'
+  }
+}
+
 export const certifications = {
   'es': {
     emittedBy: 'Emitida por: ',
