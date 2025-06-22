3de8ec9d2d69a076b2db04a501a3c80a22af22b9
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-06-22 11:39:46 -0600

    :sparkles: Init i18n translates

---
+++ src/i18n/sections.ts
@@ -0,0 +1,12 @@
+export const certifications = {
+  'es': {
+    emittedBy: 'Emitida por: ',
+    issueDate: 'Fecha de emisión: ',
+    expirationDate: 'Fecha de expiración: '
+  },
+  'en': {
+    emittedBy: 'Emitted by: ',
+    issueDate: 'Issue date: ',
+    expirationDate: 'Expiration date: '
+  },
+}
---
+++ src/i18n/titles.ts
@@ -0,0 +1,26 @@
+export const titles = {
+  'en': {
+    whoAmi: 'Who am I?',
+    whoAmiExperience: '+1 year of experience.',
+    whoAmiValue: 'IT Engineer and Full-Stack developer, specialized in creating unique web and mobile applications.',
+    experience: 'Experience',
+    projects: 'Projects',
+    aboutMe: 'About Me',
+    education: 'Education',
+    skills: 'Technical Skills',
+    hackatons: 'Contests / Hackatons',
+    certifications: 'Certifications'
+  },
+  'es': {
+    whoAmi: '¿Quién soy?',
+    whoAmiExperience: '+1 año de experiencia.',
+    whoAmiValue: 'Ingeniero en TIC\'s y desarrollador Full-Stack, especializado en crear aplicaciones web y móviles únicas.',
+    experience: 'Experiencia',
+    projects: 'Proyectos',
+    aboutMe: 'Sobre mí',
+    education: 'Educación',
+    skills: 'Habilidades Técnicas',
+    hackatons: 'Concursos / Hackatones',
+    certifications: 'Certificaciones'
+  }
+}
