3fbe79fc70a482213b1127805ff52be2236634fd
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-06-23 00:14:10 -0600

    :sparkles: Add swiper component

---
+++ src/islands/Swiper.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useRef, useState } from 'react'
+
+interface SwiperProps {
+  images: string[]
+}
+
+export function Swiper({ images }: SwiperProps) {
+  const [selectedImage, setSelectedImage] = useState(images[0] || '')
+  const scrollContainerRef = useRef<HTMLDivElement>(null)
+
+  const handleThumbnailClick = (image: string) => {
+    setSelectedImage(image)
+  }
+
+  // const scroll = (direction: 'up' | 'down') => {
+  //   if (scrollContainerRef.current) {
+  //     const scrollAmount = direction === 'up' ? -100 : 100
+  //     scrollContainerRef.current.scrollBy({ top: scrollAmount, behavior: 'smooth' })
+  //   }
+  // }
+
+  useEffect(() => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.style.maxHeight = '400px'
+    }
+  }, [])
+
+  return (
+    <div className='grid grid-cols-4 gap-4'>
+      <div className='flex flex-row lg:flex-col items-center col-span-4 lg:col-span-1'>
+        <div
+          ref={scrollContainerRef}
+          className='p-2 flex flex-row lg:flex-col gap-2 overflow-x-auto lg:overflow-y-auto no-scrollbar border border-neutral-300/60 dark:border-neutral-700/60 bg-neutral-200/80 dark:bg-neutral-800/80 rounded my-scroll'
+          style={{ maxHeight: '300px' }}
+        >
+          {images.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={`Thumbnail ${index + 1}`}
+              className={`w-full h-24 object-cover cursor-pointer rounded-md ${
+                selectedImage === image ? 'border-2 border-blue-500' : 'border border-neutral-300/60 dark:border-neutral-700/60'
+              }`}
+              onClick={() => handleThumbnailClick(image)}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className='col-span-4 lg:col-span-3'>
+        <img
+          src={selectedImage}
+          alt='Imagen principal'
+          className='w-full h-full object-cover rounded-md border border-neutral-300/60 dark:border-neutral-700/60'
+        />
+      </div>
+    </div>
+  )
+}
