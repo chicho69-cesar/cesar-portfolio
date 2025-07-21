4e7128af90ed1c49a34df46c04f76b5841a64e4b
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-07-20 21:59:09 -0600

    :sparkles: Update image swiper sizes

--- src/islands/Swiper.tsx
+++ src/islands/Swiper.tsx
@@ -6,24 +6,29 @@ interface SwiperProps {
 
 export function Swiper({ images }: SwiperProps) {
   const [selectedImage, setSelectedImage] = useState(images[0] || '')
+  const [imageHeight, setImageHeight] = useState<number | null>(null)
+
   const scrollContainerRef = useRef<HTMLDivElement>(null)
+  const previewImageRef = useRef<HTMLImageElement>(null)
 
   const handleThumbnailClick = (image: string) => {
     setSelectedImage(image)
   }
 
-  // const scroll = (direction: 'up' | 'down') => {
-  //   if (scrollContainerRef.current) {
-  //     const scrollAmount = direction === 'up' ? -100 : 100
-  //     scrollContainerRef.current.scrollBy({ top: scrollAmount, behavior: 'smooth' })
-  //   }
-  // }
-
   useEffect(() => {
-    if (scrollContainerRef.current) {
-      scrollContainerRef.current.style.maxHeight = '400px'
-    }
-  }, [])
+    if (!previewImageRef.current) return
+
+    const observer = new ResizeObserver(() => {
+      const newHeight = previewImageRef.current?.clientHeight
+      if (newHeight) {
+        setImageHeight(newHeight)
+      }
+    })
+
+    observer.observe(previewImageRef.current)
+
+    return () => observer.disconnect()
+  }, [selectedImage])
 
   return (
     <div className='grid grid-cols-5 gap-4'>
@@ -31,29 +36,30 @@ export function Swiper({ images }: SwiperProps) {
         <div
           ref={scrollContainerRef}
           className='p-2 flex flex-row lg:flex-col gap-2 overflow-x-auto lg:overflow-y-auto no-scrollbar border border-neutral-300/60 dark:border-neutral-700/60 bg-neutral-200/80 dark:bg-neutral-800/80 rounded my-scroll'
-          style={{ maxHeight: '300px' }}
+          style={{ height: imageHeight ?? 'auto' }}
         >
           {images.map((image, index) => (
             <img
               key={index}
               src={image}
               alt={`Thumbnail ${index + 1}`}
-              className={`w-full h-24 object-cover cursor-pointer rounded-md ${
-                selectedImage === image ? 'border-2 border-blue-500' : 'border border-neutral-300/60 dark:border-neutral-700/60'
-              }`}
+              className={`w-full h-24 object-contain object-center cursor-pointer rounded-md 
+                ${selectedImage === image
+                  ? 'border-2 border-blue-500'
+                  : 'border border-neutral-300/60 dark:border-neutral-700/60'
+                }`}
               onClick={() => handleThumbnailClick(image)}
             />
           ))}
         </div>
       </div>
 
-      <div className='col-span-5 lg:col-span-4'>
-        <img
-          src={selectedImage}
-          alt='Imagen principal'
-          className='w-full h-full object-cover rounded-md border border-neutral-300/60 dark:border-neutral-700/60'
-        />
-      </div>
+      <img
+        ref={previewImageRef}
+        src={selectedImage}
+        alt='Imagen principal'
+        className='block col-span-5 lg:col-span-4 object-contain rounded-md border border-neutral-300/60 dark:border-neutral-700/60 w-full'
+      />
     </div>
   )
 }
